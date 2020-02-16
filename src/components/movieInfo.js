ffb731b32085fa9372cbe2e311161010c98cd4c9
Gabriel Lopez <lopezg3000@gmail.com>
2020-02-16 11:15:42 -0600

    deleteEventSuccess

--- src/components/movieInfo.js
+++ src/components/movieInfo.js
@@ -4,13 +4,18 @@ import { getMovies } from "../services/fakeMovieService";
 
 class MovieInfo extends Component {
     state = {
-        loading: true,
-        error: "",
         data: getMovies()
     }
 
-    render() {
 
+    handleOnDelete = (id) => {
+        this.setState({
+            data: this.state.data.filter(movie => movie._id !== id)
+        })
+        console.log(this.state.data)
+    }
+
+    render() {
         return (
             <React.Fragment>
                 {this.state.data.map(movie => (
@@ -28,7 +33,7 @@ class MovieInfo extends Component {
                             <h3>{movie.dailyRentalRate}</h3>
                         </div>
                         <div className="buttonContainer">
-                            <button type="button" className="btn btn-danger" ><h3>Delete</h3></button>
+                            <button type="button" className="btn btn-danger" onClick={() => this.handleOnDelete(movie._id)}><h3>Delete</h3></button>
                         </div>
                     </React.Fragment>
                 ))}
