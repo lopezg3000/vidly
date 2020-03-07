b05dc5946dbf1102a5669d44d26479ba5a1022a0
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-06 20:33:52 -0600

    lodashlibraryInstalled

--- src/components/pagination/Pagination.js
+++ src/components/pagination/Pagination.js
@@ -1,14 +1,19 @@
 import React from "react"
 
-const Pagination = () => {
+const Pagination = props => {
 
+    const { count, pageSize } = props;
+
+    const pageCount = count / pageSize;
+
+    // [1, 2, 3].map()
 
     return (
-        <nav aria-label="Page navigation example">
+        <nav>
             <ul className="pagination">
-                <li className="page-item active" ><a className="page-link" href="#">1</a></li>
-                <li className="page-item"><a className="page-link" href="#">2</a></li>
-                <li className="page-item"><a className="page-link" href="#">3</a></li>
+                <li className="page-item">
+                    <a className="page-link">1</a>
+                </li>
             </ul>
         </nav>
     );
