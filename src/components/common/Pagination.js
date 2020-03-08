5fc2b2decd6345a9fd45623d70d593f91849f025
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-08 11:26:35 -0500

    paginationPointer

--- src/components/common/Pagination.js
+++ src/components/common/Pagination.js
@@ -16,7 +16,7 @@ const Pagination = props => {
             <ul className="pagination">
                 {pages.map(page => (
                     <li key={page} className={page === currentPage ? "page-item active" : "page-item"}>
-                        <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
+                        <a className="page-link" style={{ cursor: "pointer" }} onClick={() => onPageChange(page)}>{page}</a>
                     </li>
                 ))}
             </ul>
