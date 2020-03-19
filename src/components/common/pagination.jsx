9d91858569895fcf91f4dfccfb23322b444691b0
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-19 08:40:06 -0500

    revisedPaginateData

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -13,7 +13,7 @@ const Pagination = props => {
         <nav>
             <ul className="pagination">
                 {pages.map(page => (
-                    <li key={page} className={page === currentPage ? "page-item active" : "page-item"}>
+                    <li key={page} style={{ cursor: "pointer" }} className={page === currentPage ? "page-item active" : "page-item"}>
                         <a className="page-link" onClick={() => onPageChange(page)}>{page}</a>
                     </li>
                 ))}
--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -1,6 +1,9 @@
 import _ from "lodash";
 
 export function paginate(items, pageNumber, pageSize) {
-    const startIndex = (pageNumber - 1) * pageSize;
-    return _(items).slice(startIndex).take(pageSize).value();
+    const startIndex = (pageNumber - 1) * pageSize; //-1 to get a start index of 0 if on page 1
+    return _(items)
+        .slice(startIndex)
+        .take(pageSize) //number of items in the array taken.
+        .value(); //converts to a regular array
 }
