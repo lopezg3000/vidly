6c9c7a04d1146b53c0edd42c41da3bcc00aad044
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-21 13:53:36 -0500

    raisingSortEvent

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -16,7 +16,7 @@ class Movies extends Component {
     };
 
     componentDidMount() {
-        const genres = [{ name: 'All Genres' }, ...getGenres()];
+        const genres = [{ _id: "", name: 'All Genres' }, ...getGenres()];
 
         this.setState({ movies: getMovies(), genres });
     };
@@ -47,6 +47,10 @@ class Movies extends Component {
         this.setState({ selectedGenre: genre, currentPage: 1 });
     };
 
+    handleSort = path => {
+        console.log(path);
+    };
+
     render() {
         const { length: count } = this.state.movies;
         const { pageSize, currentPage, selectedGenre, movies: allMovies } = this.state;
@@ -71,6 +75,7 @@ class Movies extends Component {
                         movies={movies}
                         onLike={this.handleLike}
                         onDelete={this.handleDelete}
+                        onSort={this.handleSort}
                     />
                     <Pagination
                         itemsCount={filtered.length}
