11b84ee10aeaaa0977d433d642026a25d3c2bd07
Gabriel Lopez <lopezg3000@gmail.com>
2020-03-22 12:11:02 -0500

    objDestrSFC

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from "lodash"
 
-const Pagination = props => {
-    const { itemsCount, pageSize, currentPage, onPageChange } = props;
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
     // console.log("Current Page: ", currentPage);
 
     const pagesCount = Math.ceil(itemsCount / pageSize); //Math.ceil converts number calculated to an integer greater or equal to floating point number
