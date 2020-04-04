289034a37ba7cecde95ad3c02f034200c76c36e8
Gabriel Lopez <lopezg3000@gmail.com>
2020-04-04 17:02:38 -0500

    functionalNavBarExercisePart2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
 import NavBar from "./components/common/navbar";
-import Movies from './components/movies';
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 import './App.css';
 
 class App extends Component {
@@ -11,7 +15,14 @@ class App extends Component {
             <React.Fragment>
                 <NavBar />
                 <main className="container">
-                    <Movies />
+                    <Switch>
+                        <Route path="/movies" component={Movies} />
+                        <Route path="/customers" component={Customers} />
+                        <Route path="/rentals" component={Rentals} />
+                        <Route path="/not-found" component={NotFound} />
+                        <Redirect from="/" exact to="/movies" />
+                        <Redirect to="/not-found" />
+                    </Switch>
                 </main>
             </React.Fragment>
         );
