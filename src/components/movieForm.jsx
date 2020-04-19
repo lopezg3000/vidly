6a3e938f8eef2f7ddea05b241c697596cab90ad5
Gabriel Lopez <lopezg3000@gmail.com>
2020-04-18 23:17:05 -0500

    titleInputField

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Joi from 'joi-browser';
+import Form from './common/form';
 
-const MovieForm = ({ match, history }) => {
-    const { id } = match.params;
-    return (
-        <div>
-            <h1>Movie Form {id !== "new" ? id : null}</h1>
+class MovieForm extends Form {
+    state = {
+        data: { title: '' },
+        errors: {}
+    }
 
-            <button type="button" className="btn btn-primary" onClick={() => history.push("/movies")}>Save</button>
-        </div>
-    );
-};
+    schema = {
+        title: Joi.string()
+            .required()
+            .label('Title')
+    }
+
+    doSubmit = () => {
+        //Call to Server
+        console.log('Movie Saved');
+    }
+
+
+    render() {
+        const { match, history } = this.props;
+        const { id } = match.params;
+        return (
+            <div>
+                <h1>Movie Form {id !== "new" ? id : null}</h1>
+                <form onSubmit={this.handlSubmit}>
+                    {this.renderInput('title', 'Title')}
+                </form>
+                <button type="button" className="btn btn-primary" onClick={() => history.push("/movies")}>Save</button>
+            </div>
+        );
+    }
+}
 
 export default MovieForm;
