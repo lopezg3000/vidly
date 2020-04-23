bf21600037d67a6b1671b77376863811b663525a
Gabriel Lopez <lopezg3000@gmail.com>
2020-04-22 20:59:40 -0500

    componentDidMountGenresSetStateMovieIdConditionalAndDataUpdateToViewModelAdded

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -9,11 +9,11 @@ class MovieForm extends Form {
     state = {
         data: {
             title: '',
-            genreId: '', //interested in genre Id, when genre is selected in form it is the id that will be used to identify name;
+            genreId: '', //interested in genre Id, when genre is selected in form it is the id that will be used to identify name.
             numberInStock: '',
             dailyRentalRate: ''
         },
-        genres: [], //Will Change once componentDidMount is used to get genres from imaginary server
+        genres: [], //Will Change once componentDidMount is used to get genres from imaginary server.
         errors: {}
     }
 
@@ -39,7 +39,27 @@ class MovieForm extends Form {
             .min(0)
             .max(10)
             .label('Daily Rental Rate'),
-    }
+    };
+
+    componentDidMount() {
+        const genres = getGenres();
+        this.setState({ genres });
+
+        const movieId = this.props.match.params.id;
+        if (movieId === 'new') return; //return because there is no need to populate form with an existing movie object
+
+        const movie = getMovie(movieId);
+        if (!movie) return this.props.history.replace('/not-found');
+        //user will be redirected to not found page if id is not found ie wrong url.
+        //replace because pushing the back button will cause infinite loop of invalidIdPage and not-found page
+        //return keyword used so that the rest of the code is not executed. Don't assume that rest of the code will not be executed.
+        this.setState({ data: this.mapToViewModel(movie) });
+        //updating data property but not setting it to the movie object that is recieved to the server.
+        //restful api's are general purpose; not built for a specific page. Data that is returned is often used on several pages. 
+        //Each page needs a piece of that data. it is also possible that what should be displayed on the page is a little different from the structure of the data.
+        //mapToViewModel method created and used to get a movie object that is recieved from the server and maps it to a different kind of movie object that
+        //can be used on this form. movie object returned is referred to as a view model, a model with a view. 
+    };
 
     options = [
         { label: '', value: '' },
