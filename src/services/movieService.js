1904f08a279f010955da2c674ad9ee9762dbffe7
Gabriel Lopez <lopezg3000@gmail.com>
2020-05-16 15:34:37 -0500

    movieUrlFunction

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -4,24 +4,28 @@ import { apiUrl } from "../config.json";
 
 const apiEndpoint = apiUrl + "/movies";
 
+function movieUrl(id) {
+    return `${apiEndpoint}/${id}`
+}
+
 export function getMovies() {
     return http.get(apiEndpoint);
 }
 
 export function getMovie(movieId) {
-    return http.get(apiEndpoint + '/' + movieId);
+    return http.get(movieUrl(movieId));
 }
 
 export function saveMovie(movie) {
     if (movie._id) {
         const body = { ...movie };
         delete body._id;
-        return http.put(apiEndpoint + '/' + movie._id, body); //body object cannot have id.
+        return http.put(movieUrl(movie_id), body); //body object cannot have id.
     }
 
     return http.post(apiEndpoint, movie);
 }
 
 export function deleteMovie(movieId) {
-    return http.delete(apiEndpoint + '/' + movieId);
+    return http.delete(movieUrl(movieId));
 }
