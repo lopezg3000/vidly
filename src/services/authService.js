4c9f282811e560a9a459d02272eb661f57011049
Gabriel Lopez <lopezg3000@gmail.com>
2020-05-24 07:42:46 -0500

    getCurrentUserReturnsDecodedJwt

--- src/services/authService.js
+++ src/services/authService.js
@@ -12,6 +12,15 @@ export function logout() {
     return localStorage.removeItem('token');
 }
 
+export function getCurrentUser() {
+    try {
+        const jwt = localStorage.getItem('token');
+        return jwtDecode(jwt);
+    } catch (ex) {
+        return null;
+    }
+}
+
 
 export default {
     login,
