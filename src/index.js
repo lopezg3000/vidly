1178fdd7cd3f9a6c770ea51696565cede8961259
Gabriel Lopez <lopezg3000@gmail.com>
2020-05-29 11:06:19 -0500

    Environmental Production File Created

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import logger from './services/logService';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
+console.log('SUPERMAN', process.env.REACT_APP_NAME);
+
 logger.init()
 
 ReactDOM.render(
